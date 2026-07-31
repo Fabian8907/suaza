fd4a46ddd4f6a0976b8f22d83673d7153475226c
Fabian Alvarez <sistemasittech@gmail.com>
2026-07-30 21:25:11 -0500

    ajustes settings.py y passenger_wsgi.py

--- suaza/passenger_wsgi.py
+++ suaza/passenger_wsgi.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 # Indica qué archivo de configuración se debe usar
 # IMPORTANTE: Reemplaza 'nombre_de_tu_proyecto' por el nombre exacto de 
 # la carpeta que contiene tu archivo settings.py y wsgi.py
-os.environ['DJANGO_SETTINGS_MODULE'] = 'nombre_de_tu_proyecto.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'suaza.settings'
 
 # Levanta la aplicación WSGI
 from django.core.wsgi import get_wsgi_application
